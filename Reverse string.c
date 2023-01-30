57072de50ac8ba6b67194b9c8557dccea22c9816
Sathvika Ponnam <113676692+Sathvika1609@users.noreply.github.com>
2023-01-30 10:14:21 +0530

    Create Reverse string.c
    enter the string to be reversed:sathvika
    after the string is reversed:akivhtas

---
+++ Reverse string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+	char str[40];	printf("enter the string to be reversed:");
+
+	scanf("%s",str);
+
+	printf("after the string is reversed:%s",strrev(str));
+
+	return 0;
+
+}
